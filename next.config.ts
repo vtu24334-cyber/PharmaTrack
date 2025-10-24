219ff06d8ffe715dbbebc612034cdf0cfc09d0ec
vtu24334-cyber <vtu24334@veltech.edu.in>
2025-10-24 20:48:06 +0530

    edit 4

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export', // This enables static export!
   typescript: {
     ignoreBuildErrors: true,
   },
